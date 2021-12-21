17d13400331d10b13124028e0c58027f46311155
Adam Hostetter <achossy34@hotmail.com>
2021-12-20 22:01:34 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(GetReadableTime(359999));
+
+string GetReadableTime(int seconds) 
+{
+    string finaltime = "";
+
+    //Get hours
+    int hours = seconds / 3600;
+    //Convert hours to string and format with leading zero
+    string stringHours = String.Format("{0:00}", hours);
+    //Get the remainder of seconds after getting the hours
+    int remainderSecondsForMinutes = seconds % 3600;
+    //Converting remaining seconds to remaining minutes
+    int minutes = remainderSecondsForMinutes / 60;
+    //Convert minutes to string and format with leading zero
+    string stringMinutes = String.Format("{0:00}", minutes);
+    //Get the remainder of seconds after getting remaining minutes
+    int remainingSeconds = remainderSecondsForMinutes % 60;
+    //Convert remaining seconds to string and format with leading zero
+    string stringRemainingSeconds = String.Format("{0:00}", remainingSeconds);
+
+    //Combine Hours, Minutes, and Seconds to get final time
+    finaltime = stringHours + ":" + stringMinutes + ":" + stringRemainingSeconds;
+        
+    return finaltime;
+}
